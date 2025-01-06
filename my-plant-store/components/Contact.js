33e90590e5e782308317ecb41b6ca6d66f053b75
John A. Paz <johnapaz@gmail.com>
2025-01-06 18:45:29 -0500

    Initial commit

---
+++ my-plant-store/components/Contact.js
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <section className="contact-section">
+      <h2>Contact Us</h2>
+      <p>Email: johnapaz@gmail.com</p>
+      <p>Follow us on social media!</p>
+    </section>
+  );
+}
