e96c0d8a40d908791d877e25e9f6c3851c6bcf08
John A. Paz <johnapaz@gmail.com>
2025-01-06 18:40:52 -0500

    Initial commit

---
+++ my-plant-store/components/About.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <section className="about-section">
+      <h2>About Us</h2>
+      <p>We’re passionate about providing the healthiest plants and tips for your home.</p>
+    </section>
+  );
+}
