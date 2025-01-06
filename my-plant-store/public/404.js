636c91159743018371eeff2f3a31dd6d75ab5b66
John A. Paz <johnapaz@gmail.com>
2025-01-06 18:48:44 -0500

    Initial commit

---
+++ my-plant-store/public/404.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import '../styles/global.css';
+
+export default function Custom404() {
+  return (
+    <div className="error-page">
+      <h1>Oops! Page Not Found</h1>
+      <p>It looks like the page you’re looking for doesn’t exist.</p>
+      <Link href="/">
+        <button className="back-home-button">Back to Home</button>
+      </Link>
+    </div>
+  );
+}
